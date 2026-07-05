d55efb380641a616160275870b23d6ea6bcd8160
kyubeom <pllanta223@naver.com>
2026-07-05 20:04:55 +0900

    엔코더 해결, 펄스명령 작업시작

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -125,7 +125,7 @@ void MX_TIM2_Init(void)
   sConfig.IC2Polarity = TIM_ICPOLARITY_RISING;
   sConfig.IC2Selection = TIM_ICSELECTION_DIRECTTI;
   sConfig.IC2Prescaler = TIM_ICPSC_DIV1;
-  sConfig.IC2Filter = 2;
+  sConfig.IC2Filter = 4;
   if (HAL_TIM_Encoder_Init(&htim2, &sConfig) != HAL_OK)
   {
     Error_Handler();
